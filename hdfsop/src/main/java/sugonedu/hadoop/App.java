7a955cca3dda7e7ae33e418932561263b3cbcbc8
moylaugh <moylaugh@gmail.com>
2018-10-17 11:49:01 +0800

    hdfs operation: old api

--- hdfsop/src/main/java/sugonedu/hadoop/App.java
+++ hdfsop/src/main/java/sugonedu/hadoop/App.java
@@ -1,36 +1,46 @@
 package sugonedu.hadoop;
 
-import org.apache.hadoop.fs.*;
-import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
 import java.io.*;
-import java.util.EnumSet;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        FileContext fc = FileContext.getFileContext();
-        String home_string = System.getProperty("user.home");
-        Path home_path = new Path(home_string);
-        Path password_path = new Path(home_path, "moyi/fstest/password");
-        try {
-            fc.delete(password_path, false);
-        } catch (FileNotFoundException e) {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+        Path hdfsPath = new Path("/home/ua36/moyi/fstest/password");
+        if (fs.exists(hdfsPath)) {
+            fs.delete(hdfsPath, false);
         }
-        try {
-            fc.mkdir(password_path.getParent(),
-                    FsPermission.getDirDefault(), true);
-        } catch (FileAlreadyExistsException e) {
+
+        if (!fs.exists(hdfsPath.getParent())) {
+            fs.mkdirs(hdfsPath.getParent());
         }
-        EnumSet<CreateFlag> flags =  EnumSet.of(CreateFlag.CREATE);
-        try (
-                InputStream in = new FileInputStream("/etc/passwd");
-                OutputStream out = fc.create(password_path, flags)) {
-            IOUtils.copyBytes(in, out, 8192);
+        InputStream in = new BufferedInputStream(
+                new FileInputStream("/etc/passwd"));
+        OutputStream out = fs.create(hdfsPath);
+        IOUtils.copyBytes(in, out, 4096, true);
+
+        try (FSDataOutputStream append = fs.append(hdfsPath)) {
+            append.write("Hello HDFS\n".getBytes());
         }
-        try (FSDataOutputStream out = fc.create(password_path,
-                EnumSet.of(CreateFlag.APPEND))) {
-            out.write("Hello HDFS\n".getBytes("utf-8"));
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                new BufferedInputStream(fs.open(hdfsPath))))) {
+            String line = null;
+            int count = 0;
+            while ((line = reader.readLine()) != null) {
+                count++;
+                if (line.contains("daemon")) {
+                    System.out.println(line);
+                }
+            }
+            System.out.println(count);
         }
     }
 }
